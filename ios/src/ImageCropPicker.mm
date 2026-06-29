28ce695b73c716d13e4f19d2c46603bfdae4efd4
FCG <143424305+FCG-labs@users.noreply.github.com>
2026-06-29 14:21:53 +0900

    🐛: iOS atflee 크롭 원본 해상도 보존
    Why: atfleeCropper 경로가 iOS 기본 width/height 리사이즈를 타면 크롭 결과가 낮은 해상도로 저장될 수 있음. (@/Users/fcg_dev01/project/atflee/app/rn_module/atflee-react-native-image-crop-picker/ios/src/ImageCropPicker.mm:848-863)
    
    What: atfleeCropper에서는 croppedImage를 그대로 compression 단계로 넘기고, legacy cropper에서만 width/height 출력 박스 리사이즈를 유지함.
    
    Proof: git diff --cached --check 통과.
    
    Risk/Rollback: 해당 커밋 또는 v0.51.1-atflee.5 태그 적용을 되돌리면 기존 cropper 동작으로 복귀.
    
    Co-authored-by: FCG <143424305+FCG-labs@users.noreply.github.com>

--- ios/src/ImageCropPicker.mm
+++ ios/src/ImageCropPicker.mm
@@ -849,15 +849,19 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
 - (void)imageCropViewController:(UIViewController *)controller
                    didCropImage:(UIImage *)croppedImage
                   usingCropRect:(CGRect)cropRect {
-    
-    // we have correct rect, but not correct dimensions
-    // so resize image
-    CGSize desiredImageSize = CGSizeMake([[[self options] objectForKey:@"width"] intValue],
-                                         [[[self options] objectForKey:@"height"] intValue]);
-    
-    UIImage *resizedImage = [croppedImage resizedImageToFitInSize:desiredImageSize scaleIfSmaller:YES];
-    ImageResult *imageResult = [self.compression compressImage:resizedImage withOptions:self.options];
-    
+
+    BOOL useAtfleeCropper = [[[self options] objectForKey:@"atfleeCropper"] boolValue];
+    UIImage *imageToPersist = croppedImage;
+
+    if (!useAtfleeCropper) {
+        // Legacy cropper mode expects width/height to define the final output box.
+        CGSize desiredImageSize = CGSizeMake([[[self options] objectForKey:@"width"] intValue],
+                                             [[[self options] objectForKey:@"height"] intValue]);
+        imageToPersist = [croppedImage resizedImageToFitInSize:desiredImageSize scaleIfSmaller:YES];
+    }
+
+    ImageResult *imageResult = [self.compression compressImage:imageToPersist withOptions:self.options];
+
     NSString *filePath = [self persistFile:imageResult.data];
     if (filePath == nil) {
         [self dismissCropper:controller selectionDone:YES completion:[self waitAnimationEnd:^{
